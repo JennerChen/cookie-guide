3cacf225b01b266c4c98166ff2fe3a8adbbd5bc1
zhangqing <zhangqing332@live.com>
2020-11-29 14:48:15 +0800

    feat: 服务端设置cookie和httpOnly

--- server/index.js
+++ server/index.js
@@ -11,6 +11,14 @@ app.get('/', (req, res) => {
     res.send(content)
 })
 
+app.get('/no-document-cookie', () => {
+    const content = `
+        <h2>don't allow document.cookie</h2>
+    `
+    res.set('Set-Cookie', 'mycookie2=beaf;HttpOnly')
+    res.send(content)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
