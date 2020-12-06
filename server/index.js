afb28b8adb3bd5bba75a18397bbaad501bd14514
zhangqing <zhangqing332@live.com>
2020-12-06 14:48:32 +0800

    feat: scope path 介绍

--- server/index.js
+++ server/index.js
@@ -22,20 +22,22 @@ app.get('/no-document-cookie', (req, res) => {
 app.get('/pathA', (req, res) => {
     const content = `
         <h2>set only pathA</h2>
-        <h2>you only can see scopeValue in /pathA</h2>
+        <h2>you only can see scopeValueA in /pathA</h2>
     `
-    res.set('Set-Cookie', 'scopeValue=pathA;path=/pathA')
+    res.set('Set-Cookie', 'scopeValueA=pathA;path=/pathA')
     res.send(content)
 })
 
 app.get('/pathB', (req, res) => {
     const content = `
         <h2>set only pathA</h2>
-        <h2>you only can see scopeValue in /pathB</h2>
+        <h2>you only can see scopeValueB in /pathB</h2>
     `
-    res.set('Set-Cookie', 'scopeValue=pathA;path=/pathB')
+    res.set('Set-Cookie', 'scopeValueB=pathB;path=/pathB')
     res.send(content)
 })
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
